3aa30be05bb085d3019d8b5f7917a380527ce856
mac <963390654@qq.com>
2018-03-12 11:54:42 +0800

    9

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello.java{
 	public static void main(String[] args){
-		System.out.print("hell git,heihei”);
-		System.out.print(“你很棒哦“);
+		System.out.print("hell git,heihei2232”);
+		System.out.print(“你很棒哦553453“);
 	}
 }
